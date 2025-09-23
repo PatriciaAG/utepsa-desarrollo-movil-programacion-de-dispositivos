c3e4f6007b76196faf53dfa8de7da0f0921f9db7
hananelio <619824@north.systems>
2025-09-23 19:57:24 -0400

    Inidiando proyecto cámara

---
+++ src/app/pages/registro-persona/registro-persona.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-persona',
+  templateUrl: './registro-persona.component.html',
+  styleUrls: ['./registro-persona.component.scss'],
+  standalone: true,
+})
+export class RegistroPersonaComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
