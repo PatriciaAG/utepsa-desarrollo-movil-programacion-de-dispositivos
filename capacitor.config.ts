792deb7a72830dc3ac85dc6a873f783d9ffde223
Ariel <arielalgaranaz06@gmail.com>
2025-09-24 19:00:34 -0400

    Commit 24/09

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'practica-camara-gps',
+  appId: 'com.utepsa.practicacamaragps',  
+  appName: 'Proyecto App Movil',          
   webDir: 'www'
 };
 
