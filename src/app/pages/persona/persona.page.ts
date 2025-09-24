9496d3e7216caed6b2aab6ed31a0da2659a3c8da
Diego <diegoguzman366@.com>
2025-09-23 21:12:08 -0400

    Se agrega una pantalla para la camara

--- src/app/pages/persona/persona.page.ts
+++ src/app/pages/persona/persona.page.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonToolbar } from '@ionic/angular/standalone';
+import { RegistroPersonaComponent } from '../registro-persona/registro-persona.component';
 
 @Component({
   selector: 'app-persona',
   templateUrl: './persona.page.html',
   styleUrls: ['./persona.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonToolbar, CommonModule, FormsModule, RegistroPersonaComponent]
 })
 export class PersonaPage implements OnInit {
 
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, ExploreContainerComponent, RouterLink],
 })
 export class Tab1Page {
   constructor() {}
